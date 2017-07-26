c16a0cad6847fb3d1d195c6e0daa6b54bd7af614
jinjingyuan <jinjigyuan@huoqiu.cn>
2017-07-26 16:32:00 +0800

    first commit

---
+++ SelectionCell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SelectionCell
+//
+//  Created by 金靖媛 on 2017/7/26.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
